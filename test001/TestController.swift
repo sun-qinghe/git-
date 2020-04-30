4cc7790fbaec33aa1acb32398c60265cdfce5367
sun-qinghe <lucky_jonas@yeah.net>
2020-04-30 15:22:25 +0800

    增加vc

---
+++ test001/TestController.swift
@@ -0,0 +1,30 @@
+//
+//  TestController.swift
+//  test001
+//
+//  Created by 安中 on 2020/4/30.
+//  Copyright © 2020 az. All rights reserved.
+//
+
+import UIKit
+
+class TestController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("1111111111111")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
